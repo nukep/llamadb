dad4ff5a2e159d2b942b870c3ab17fcbe903e15f
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2016-12-20 22:59:53 -0800

    Replace Duration::span with Instant::now().elapsed

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,3 @@
-#![feature(duration_span)]
-
 #[macro_use]
 extern crate log;
 
@@ -9,7 +7,7 @@ extern crate linenoise;
 extern crate llamadb;
 
 use std::io::Write;
-use std::time::Duration;
+use std::time::Instant;
 
 mod prettyselect;
 use prettyselect::pretty_select;
@@ -82,12 +80,10 @@ fn execute_statement(out: &mut Write, db: &mut llamadb::tempdb::TempDb, statemen
 {
     use llamadb::tempdb::ExecuteStatementResponse;
 
-    let mut execute_result = None;
+    let now = Instant::now();
+    let execute_result = Some(db.execute_statement(statement));
+    let duration = now.elapsed();
 
-    let duration = Duration::span(|| {
-        execute_result = Some(db.execute_statement(statement));
-    });
-    
     let seconds = duration.as_secs() as f32 + (duration.subsec_nanos() as f32 * 1.0e-9);
 
     let duration_string = format!("{:.3}s", seconds);
